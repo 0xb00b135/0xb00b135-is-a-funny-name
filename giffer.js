4af25fed689538b65d09d508a8c6cb3281335b78
0xb00b135 <167912255+0xb00b135@users.noreply.github.com>
2024-04-26 10:43:44 +0200

    Update giffer.js

--- giffer.js
+++ giffer.js
@@ -3,13 +3,19 @@ function poopup() {
         const clone = element.cloneNode(true);
         element.parentNode.replaceChild(clone, element);
     });
-    
-    const overlay = document.createElement("iframe");
+
+    const overlay = document.createElement("img");
     overlay.style.position = "fixed";
     overlay.style.top = "0";
     overlay.style.left = "0";
     overlay.style.width = "100%";
     overlay.style.height = "100%";
+    var url = " \
+    https://cdn.discordapp.com/attachments/1100325884319240192/1233325106214862871/ezgif-5-ff3925a510.gif?ex=662caf03&is=662b5d83&hm=9fbce3520da77d4687b0f3bc1039862923e20ed4b00246ed0df7259d0e5ee03a& \
+    "
+    overlay.src = url;
+    overlay.frameborder="0";
+    overlay.style.zIndex = "32000";
     document.body.append(overlay);
 }
 
