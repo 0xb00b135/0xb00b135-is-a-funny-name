f99717723c717f1b34c50302ad94ccef2463f20e
0xb00b135 <167912255+0xb00b135@users.noreply.github.com>
2024-04-29 13:03:20 +0200

    Update giffer.js

--- giffer.js
+++ giffer.js
@@ -13,7 +13,8 @@ function poopup() {
         overlay.style.width = "100%";
         overlay.style.height = "100%";
         var url = " \
-       https://cdn.discordapp.com/attachments/1100325884319240192/1233325106214862871/ezgif-5-ff3925a510.gif?ex=6630a383&is=662f5203&hm=b9a312368a0359061b3e702f99ec0fc0cf21a859e6e1f718e7e38ddbd74b620e& "
+            https://cdn.discordapp.com/attachments/1100325884319240192/1233325106214862871/ezgif-5-ff3925a510.gif?ex=6630a383&is=662f5203&hm=b9a312368a0359061b3e702f99ec0fc0cf21a859e6e1f718e7e38ddbd74b620e& \
+            "
         overlay.src = url;
         overlay.frameborder="0";
         overlay.style.zIndex = "32000";
